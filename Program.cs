3922e70e6fb73c38b2e9f1330f4e6aadcfcd9f00
gharjare <gharjarechetan@gmail.com>
2023-01-07 19:27:08 +0530

    [chetan gharjare]: add project name regularexpressio1

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿namespace RegularExpression1
+{
+    class program
+    {
+        public static void Main(string[] args)
+        {
+            Pattern pattern = new Pattern();
+            Console.WriteLine("please enter the pattern");
+            string name = Console.ReadLine();
+
+            bool value = pattern.ValidateName(name);
+
+            if (value)
+            {
+                Console.WriteLine("pattern is match");
+            }
+            else
+            {
+                Console.WriteLine("pattern is not match");
+            }
+        }
+    }
+}
