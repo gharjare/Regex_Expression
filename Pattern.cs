79da6dd1991b94d8cec1b85d7e0771d6b4689136
gharjare <gharjarechetan@gmail.com>
2023-01-09 16:32:01 +0530

    [chetan gharjare]:here we write code for passing test case

--- Pattern.cs
+++ Pattern.cs
@@ -9,10 +9,97 @@ namespace RegularExpression1
 {
     public class Pattern
     {
-        public static string Regex_Character = "((?=.*[A-Z])(?=.*[0-9])(?=.*[@#$%^&*_]).{8,})";
-        public bool ValidateCharacter(string Character)
+        public static string ValidateFirstName(string firstName)
         {
-            return Regex.IsMatch(Character, Regex_Character);
+            Regex regex = new Regex("^[A-Z][a-z]{2,}$");
+            bool res = regex.IsMatch(firstName);
+            if (res)
+            {
+                Console.WriteLine("Valid");
+                return firstName;
+            }
+            else
+            {
+                Console.WriteLine("Invalid");
+            }
+            return default;
+        }
+        public static string ValidateLName(string lastName)
+        {
+            string pattern = "^[A-Z][a-z]{2,}$";
+            Regex regex = new Regex(pattern);
+            bool res1 = regex.IsMatch(lastName);
+            if (res1)
+            {
+                Console.WriteLine("Valid");
+                return lastName;
+            }
+            else
+            {
+                Console.WriteLine("Invalid");
+            }
+            return default;
+
+        }
+        public static string ValidateEmail(string email)
+        {
+
+            string emailPattern = (@"^[a-zA-Z0-9]+([\.\+\-][a-zA-Z0-9]+)?@[a-zA-Z0-9-]+(\.[a-zA-Z]{2,}(\.[a-zA-Z]+)?)$");
+            Regex regex = new Regex(emailPattern);
+           
+            bool result = regex.IsMatch(email);
+            if (result)
+            {
+                Console.WriteLine(email + " ----->Valid");
+                return email;
+            }
+            else
+            {
+                Console.WriteLine(email + " ----->Invalid");
+            }
+
+            return default;
+        }
+
+        public static string ValidatePhoneNum(string phoneNum)
+        {
+            string[] phoneNumInput = { "91 7852234896", " 91 9865741548", "919865795312", "91@123", "A865" };
+            string phoneNumPattern = @"^[0-9]+[\s]+[0-9]{10}$";
+            Regex regex = new Regex(phoneNumPattern);
+
+            bool result = regex.IsMatch(phoneNum);
+            if (result)
+            {
+                Console.WriteLine(phoneNum + " ----->Valid");
+                return phoneNum;
+            }
+            else
+            {
+                Console.WriteLine(phoneNum + " ----->InValid");
+            }
+            return default;
+        }
+
+
+        public static string ValidatePassWord(string password)
+        {
+            string[] passwordInput = { "Chetan@12", "CheTan_32", "rahul123", "kunal" };
+            string passwordPattern = @"(?=.*[A-Z])(?=.*[a-z])(?=.*[0-9])(?!.*[<>`])(?=[^.,:;'!@#$%^&*_+=|(){}[?\-\]\/\\]*[.,:;'!@#$%^&*_+=|(){}[?\-\]\/\\][^.,:;'!@#$%^&*_+=|(){}[?\-\]\/\\]*$).{8,}$";
+
+            Regex regex = new Regex(passwordPattern);
+
+            bool result = regex.IsMatch(password);
+            if (result)
+            {
+                Console.WriteLine(password + " ----->Valid");
+                return password;
+            }
+            else
+            {
+                Console.WriteLine(password + " ----->InValid");
+            }
+
+            return default;
         }
     }
 }
